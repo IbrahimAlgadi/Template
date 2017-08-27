2c2ba163249a3ef818eaff6c0950db94827e6f89
Unknown <mustafa.alfakih@gmail.com>
2017-08-27 22:47:34 +0300

    Mustafa
    New look and categorization for the clinic

---
+++ bowakeer/public/pharmacy.php
@@ -0,0 +1,39 @@
+<?php require_once("../includes/initialize.php"); ?>
+<!--RFC : Only Change the class-->
+
+<?php
+	include_layout_template('header.php');
+?>
+
+<header class="w3-container w3-theme" style="padding:21px 32px">
+  <h1 class="w3-xlarge">3RP System</h1>
+  
+  <div class="w3-row">
+    <div class="w3-col m12">
+    <div class="w3-bar">
+   <a href="pharmacy.php" class="w3-bar-item w3-button w3-round-medium">Home</a>
+   <a href="medicines.php" class="w3-bar-item w3-button w3-round-medium">Medicines</a>
+   <a href="pharm_categories.php" class="w3-bar-item w3-button w3-round-medium">Categories</a>
+</div> 
+</div>
+    </div>
+  
+</header>
+
+
+<div class="w3-container" style="padding:0px;">
+
+    
+    <div class="w3-row">
+
+    <div class="w3-col m12" style="">
+	<img src="images/pharmacy.jpg" style="width:100%">
+	
+    </div>
+    
+</div>
+
+<?php
+	include_layout_template('footer.php');
+?>
+     
